e1fd4dc9e1181e3968fdc5dd0872d809d473f0c5
Grigoriy <grigoriykosarev@gmail.com>
2017-11-08 17:44:37 +0200

    згадуєм конект

--- connect_db.php
+++ connect_db.php
@@ -1,14 +1,28 @@
 <?php
 
+    $dsn = 'mysql:dbname=mikro_blog;host=127.0.0.1;charset=utf8';
+    $user = 'root';
+    $password = '';
+
     try {
-        $pdo = new PDO(
-            'mysql:host=lesson08-mysql;dbname=mikro_blog',
-            'root',
-            '',
-            [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
-    }
-    catch (PDOException $e){
-        echo "Невдалось встановити зэднання з б.д.";
+
+        $pdo = new PDO($dsn, $user, $password);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    } catch (PDOException $e) {
+        if ($e->getCode() === 1049){
+
+            try {
+
+                $conn = new PDO('mysql:dbname=mysql;host=127.0.0.1;charset=utf8', $user, $password);
+                $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                $sql = "CREATE DATABASE mikro_blog";
+                $conn->exec($sql);
+            } catch (PDOException $r){
+                echo 'Подключение не удалось: ' . $r->getMessage();
+            }
+        }
+
     }
 
 ?>
--- create_dropp_tables_db.php
+++ create_dropp_tables_db.php
@@ -1,5 +1,9 @@
+<head>
+    <meta charset="UTF-8">
+</head>
+
 <?php
 
-
+    include 'connect_db.php';
 
 ?>
